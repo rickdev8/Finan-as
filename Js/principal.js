13bdb10d38f16ba4576533ccf9d7485a79585519
rickdev8 <rique88858@gmail.com>
2024-12-05 14:40:40 -0300

    Update principal.js

--- Js/principal.js
+++ Js/principal.js
@@ -125,7 +125,6 @@ async function edit(transacoest) {
 
   const response = await fetch(`https://api-k7y1.onrender.com/usuarios/${idf}`, {
     method: 'PUT',
-    mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json',
     },
@@ -145,7 +144,6 @@ async function editartransacao(novaTransacao){
 
   const response = await fetch(`https://api-k7y1.onrender.com/usuarios/${userId}/${transacaoId}`, {
     method: 'PUT',
-    mode: 'no-cors',
     headers: {
         'Content-Type': 'application/json',
     },
@@ -271,7 +269,6 @@ async function deleteTransacao(userId, transacaoId) {
   try {
     const response = await fetch(`https://api-k7y1.onrender.com/usuarios/${userId}`, {
       method: 'DELETE',
-      mode: 'no-cors',
       headers: {
           'Content-Type': 'application/json',
       },
